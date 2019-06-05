5d995892bf25393989a777ddf591003ec7e99306
matthras <matthew.y.p.mack@gmail.com>
2019-06-05 12:18:50 +1000

    Revert "installed isomorphic-unfetch"
    This reverts commit 57ddc003c4c4999b04ba1ae0793f685800eec2d0.

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,5 @@
-import fetch from 'isomorphic-unfetch';
-
-const Index = props => (
+export default () => (
   <div>
-    <h1>Derp, here's the data</h1>
-    {data}
+    <p>Hello World!</p>
   </div>
 )
-
-Index.getInitialProps = async function() {
-  const res = await fetch('https://matthras.zendesk.com/api/v2/tickets.json', {
-      "method": "GET",
-      headers: {
-        "Authorization": "Basic bWF0dGhldy55LnAubWFja0BnbWFpbC5jb206eldjYWV4cWRa"
-      }
-    });
-  const data = await res.json();
-  return {
-    data: JSON.stringify(data)
-  }
-}
-
-
-export default Index;
