d52549216e8de07016cdc5ac892b5d1120f32324
matthras <matthew.y.p.mack@gmail.com>
2019-06-05 12:30:58 +1000

    implemented working API request with logging

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,25 @@
-export default () => (
+import fetch from 'isomorphic-unfetch';
+
+const Index = props => (
   <div>
-    <p>Hello World!</p>
+    <h1>Derp, here's the data</h1>
+    {data}
   </div>
 )
+Index.getInitialProps = async function() {
+  const res = await fetch('https://matthras.zendesk.com/api/v2/tickets.json', {
+      "method": "GET",
+      headers: {
+        "Authorization": "Basic bWF0dGhldy55LnAubWFja0BnbWFpbC5jb206eldjYWV4cWRa"
+      }
+    });
+  console.log(typeof res);
+  console.log(res);
+  const data = await res.json();
+  console.log(data);
+  return {
+    data: data
+  }
+}
+
+export default Index; 
