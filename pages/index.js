b74d3fb3a1f32a68d222f34129196fbdd88b8b2c
matthras <matthew.y.p.mack@gmail.com>
2019-06-12 20:27:02 +1000

    refactored Index into a functioning React component

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,40 @@
 import fetch from 'isomorphic-unfetch';
+import React from 'react';
+import Error from 'next/error';
 
-const Index = props => (
-  <div>
-    <h1>Derp, here's the data</h1>
-    <ul>
-      {props.data.tickets.map(ticket => (
-        <li key={ticket.id}>
-          {ticket.subject}
-        </li>
-      ))}
-    </ul>
-  </div>
-)
-Index.getInitialProps = async function() {
-  const authToken = Buffer.from(process.env.EMAIL+':'+process.env.PASSWORD).toString('base64'); // Node version of btoa()
-  const res = await fetch('https://matthras.zendesk.com/api/v2/tickets.json', {
+class Index extends React.Component {
+  static async getInitialProps() {
+    const authToken = Buffer.from(process.env.EMAIL+':'+process.env.PASSWORD).toString('base64'); // Node version of btoa()
+    const res = await fetch('https://matthras.zendesk.com/api/v2/tickets.json', {
       "method": "GET",
       headers: {
         "Authorization": `Basic ${authToken}`
       }
     });
-  const data = await res.json();
-  return {
-    data: data
+    const errorCode = res.statusCode > 200 ? res.statusCode : false;
+    const data = await res.json();
+    return {
+      errorCode,
+      data: data
+    }
+  }
+  render() {
+    if(this.props.errorCode) {
+      return <Error statusCode={this.props.errorCode} />
+    }
+
+    return(
+      <div>
+        <h1>Derp, here's the data</h1>
+        <ul>
+          {this.props.data.tickets.map(ticket => (
+            <li key={ticket.id}>
+            {ticket.subject}
+            </li>
+          ))}
+        </ul>
+      </div>
+    ) 
   }
 }
 
