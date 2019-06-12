429b7eeef2614d6a6ce18cb1b8689bc76b1aca96
matthras <matthew.y.p.mack@gmail.com>
2019-06-12 22:59:37 +1000

    added (not-working) API response tests

--- __tests__/api_response_test.js
+++ __tests__/api_response_test.js
@@ -1,13 +1,24 @@
+/* eslint-env jest */
+
+import React from 'react'
+import { render } from 'react-testing-library'
+import Index from '../pages/index.js'
+
 // Series of tests that account for all the different possible responses from calling the API e.g. HTTP 200, 401, etc. and ensures the component renders as indicated.
-// Right now I don't know which are the main ones and which others to shuffle into a more general catch-all. Currently going by: https://www.restapitutorial.com/httpstatuscodes.html
+// Note: None of these tests are working and I ran out of time :(
+describe('API HTTP Response: 200', () => {
+  it('renders title "ZenDesk Ticket Data"', () => {
+    const { getByText } = render(<Index errorCode='200' />);
 
-// Response Status: 200 (OK) - Test that...it renders something? 
+    expect(getByText('ZenDesk Ticket Data')).toBeInTheDocument()
+  })
+})
 
-// Response Status: 204 (No content)
-// Return "Error: There is no content to display"
-
-// Response Status: 401 (Unauthorised) - Returns { error: 'Couldn\'t authenticate you' }
-// Return "Error: Your details couldn't be authenticated."
-
-// Response Status: 500 (Internal Server Error)
-// Return "Error: There is a problem with the API making it inaccessible right now."
+describe('API HTTP Response: >200', () => {
+  it('match Error component snapshot"', () => {
+    const { asFragment } = render(
+      <Error statusCode={this.props.errorCode} />
+    )
+    expect(asFragment()).toMatchSnapshot()
+  })
+})
